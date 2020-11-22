8b7596c3ef381fa0278383885c969974cfb433da
satiriorn <satiriorn@gmail.com>
2020-11-22 10:03:24 +0200

    refactoring

--- badge.h
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-
-char ssid[] = "TP-LINK17";     // your network SSID (name)
-char password[] = "0507092767"; // your network key
-String chat_id = "397362619";
-#define BOTtoken "628590242:AAEVn6EKF3LAYGVoq9Kcxmzuxx75CzGHv-0"
-String token = BOTtoken;
